0676851179772b76583575bf0513155f9e45e242
Martin Coplák <martin.coplak@gmail.com>
2023-01-06 16:17:20 +0100

    Initialized gradle setup, with Kotlin 1.5.31 and Java 8
    Signed-off-by: Martin Coplák <martin.coplak@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,35 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
-    application
+    kotlin("jvm") version "1.5.31"
+    java
 }
 
-group = "com.misuda12"
-version = "1.0-SNAPSHOT"
+group       = "com.github.misuda12"
+version     = "1.0-SNAPSHOT"
+description = ""
 
-repositories {
-    mavenCentral()
-}
+allprojects {
+    repositories {
+        maven { url = uri("https://repo.aikar.co/content/groups/aikar/") }
+        maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
+        maven { url = uri("https://jitpack.io") }
+        mavenCentral()
+    }
 
-dependencies {
-    testImplementation(kotlin("test"))
+    dependencies {
+        compileOnly(kotlin("stdlib"))
+        testImplementation(kotlin("test"))
+    }
+
+    kotlin {
+        sourceSets.all {
+            with(languageSettings) {
+                progressiveMode = true
+                enableLanguageFeature("InlineClasses")
+            }
+        }
+    }
 }
 
 tasks.test {
@@ -24,6 +40,26 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-application {
-    mainClass.set("MainKt")
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+    options.isFork = true
+    options.forkOptions.executable = "javac"
+    options.release.set(8)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        javaParameters = true
+    }
+}
+
+subprojects {
+    tasks {
+        creating(Jar::class) {
+            from(sourceSets["main"].allSource)
+            archiveClassifier.set("sources")
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
-
 rootProject.name = "PowerBlocks"
 
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+        val shadowVersion: String by settings
+        id("com.github.johnrengelman.shadow") version shadowVersion
+        kotlin("jvm") version kotlinVersion
+    }
+}
